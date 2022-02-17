6f2bf15db7708f459bc2707f23814d8fa5ec0926
King Ponpawit Paoseng <ponpawit@livingmobile.me>
2022-02-17 14:20:42 +0700

    Add Bilibili Big Player

---
+++ BiliBlilBigPlayer.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Bilibili Big Player
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  BIG PLAYER click "B" Button in player
+// @author       king-ppap
+// @match        https://www.bilibili.tv/th/play/*
+// @icon         https://p.bstarstatic.com/fe-static/deps/bilibili_tv.ico
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  let bigState = false;
+  let isAddedBtn = true;
+  let interV = setInterval(() => {
+    const playerBar = document.getElementsByClassName("player-mobile-control-bar-right")[0];
+    if (playerBar) {
+      document.getElementsByClassName("player-mobile-control-bar-right")[0].innerHTML += `<div>
+          <button id="playBigq" style="
+              padding: 10px;
+              background: #0c3d878c;
+              color: #fff;
+              border-radius: 25px;
+          ">B</button>
+      </div>`;
+      document.getElementById("playBigq").addEventListener('click', () => {
+        const bilibiliPlayer = document.getElementById("bilibiliPlayer");
+        if (!bigState) {
+          bilibiliPlayer.style.zIndex = "999";
+          bilibiliPlayer.style.position = "fixed";
+          bilibiliPlayer.style.left = "0";
+          bilibiliPlayer.style.top = "0";
+        } else {
+          bilibiliPlayer.style.zIndex = "";
+          bilibiliPlayer.style.position = "";
+          bilibiliPlayer.style.left = "";
+          bilibiliPlayer.style.top = "";
+        }
+        bigState = !bigState
+      });
+      clearInterval(interV);
+    }
+  }, 100);
+  document.getElementsByClassName("main-area media-size__video-wrap")[0].style.width = "100%";
+  document.getElementsByClassName("episode-area select-ep")[0].style.width = "100%";
+  document.getElementsByClassName("episode-area select-ep")[0].style.height = "450px";
+
+  const tempPlayer = document.getElementsByClassName("media-size__video");
+  for (let index = 0; index < tempPlayer.length; index++) {
+    tempPlayer[index].style.height = "80vh";
+    tempPlayer[index].style.width = "100%";
+  }
+})();
