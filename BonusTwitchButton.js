d51a554a6fc7536b4691563f72a6c6a2b361a015
Ponpawit Paoseng <kingponpawit@gmail.com>
2022-02-25 21:06:09 +0700

    Update BonusTwitchButton
    Add downloadURL and updateURL

--- BonusTwitchButton.js
+++ BonusTwitchButton.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Bonus Twitch Button
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.0.1
 // @description  Auto Click Bonus Twitch Button
 // @author       king-ppap
 // @match        https://www.twitch.tv/*
 // @icon         https://static.twitchcdn.net/assets/favicon-16-52e571ffea063af7a7f4.png
 // @grant        none
+// @downloadURL  https://raw.githubusercontent.com/king-ppap/scripts-tampermonkey/main/BonusTwitchButton.js
+// @updateURL    https://raw.githubusercontent.com/king-ppap/scripts-tampermonkey/main/BonusTwitchButton.js
 // ==/UserScript==
 
 (function () {
