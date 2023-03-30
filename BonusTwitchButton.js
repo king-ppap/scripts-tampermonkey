cbfe5e79977b543a8951d62d5b32e70570aaef16
Ponpawit Paoseng <kingponpawit@gmail.com>
2023-03-31 04:00:30 +0700

    Update BonusTwitchButton.js

--- BonusTwitchButton.js
+++ BonusTwitchButton.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bonus Twitch Button
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @description  Auto Click Bonus Twitch Button
 // @author       king-ppap
 // @match        https://www.twitch.tv/*
@@ -18,9 +18,9 @@
   let count = 0, waiting = 0, histo = [];
   setInterval(
       () => {
-          let temp = document.querySelector('[d="M16.503 3.257L18 7v11H2V7l1.497-3.743A2 2 0 015.354 2h9.292a2 2 0 011.857 1.257zM5.354 4h9.292l1.2 3H4.154l1.2-3zM4 9v7h12V9h-3v4H7V9H4zm7 0v2H9V9h2z"]');
+          let temp = document.querySelector('.kIlsPe');
           if (temp) {
-              temp.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.click();
+              temp.click();
               const timeTemp = (waiting*SET_TIME)/1000;
               console.log(`${new Date().toISOString()}Get bonus for ${++count} times AND waiting for ${timeTemp} sec.`);
               histo.push(timeTemp);
